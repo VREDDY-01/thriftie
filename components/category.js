162bf157597b7cccf872ca56812b6e871254a0f0
K Vishnu Teja <vreddy2300@gmail.com>
2023-12-29 09:31:08 +0530

    StoreFront page design-1

---
+++ components/category.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+const Category = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-20 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/4 lg:w-1/5 w-1/2">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="https://dummyimage.com/500x500"
+                  alt="blog"
+                />
+                <div className="p-1 text-center">
+                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                    SHIRTS
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 lg:w-1/5 w-1/2">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="https://dummyimage.com/500x500"
+                  alt="blog"
+                />
+                <div className="p-1 text-center">
+                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                    BOTTOMS
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 lg:w-1/5 w-1/2">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="https://dummyimage.com/500x500"
+                  alt="blog"
+                />
+                <div className="p-1 text-center">
+                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                    JACKETS
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 lg:w-1/5 w-1/2">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="https://dummyimage.com/500x500"
+                  alt="blog"
+                />
+                <div className="p-1 text-center">
+                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                    T-SHIRTS
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 lg:w-1/5 w-1/2">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <img
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="https://dummyimage.com/500x500"
+                  alt="blog"
+                />
+                <div className="p-1 text-center">
+                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                    T-SHIRTS
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Category;
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head><link
+          href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"
+          rel="stylesheet"
+        /></Head>
       <body>
         <Main />
         <NextScript />
       </body>
+      <Script src="../path/to/flowbite/dist/flowbite.min.js"></Script>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></Script>
     </Html>
   )
 }
