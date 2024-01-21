a0e6a40ed6915545e4723901e729a46651bfe92d
K Vishnu Teja <vreddy2300@gmail.com>
2024-01-21 11:38:00 +0530

    SideBar Fix Bug

--- components/Sidebar/sidebar.js
+++ components/Sidebar/sidebar.js
@@ -50,7 +50,7 @@ const Sidebar = ({ logout }) => {
 
       <aside
         id="logo-sidebar"
-        className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-white border-r border-gray-200 block ssmx:${menu} dark:bg-gray-800 dark:border-gray-700`}
+        className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-white border-r border-gray-200 md:block ${menu} dark:bg-gray-800 dark:border-gray-700`}
       >
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
